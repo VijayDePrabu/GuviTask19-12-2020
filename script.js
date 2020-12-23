df9c1f3e094710643fa4220cc8bef0bb9b86aef8
Vijay De Prabu <vijayde3@gmail.com>
2020-12-23 23:02:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,82 @@
+// * Print odd numbers in an array 
+let numArray = [3,76,24,66,37,21,34,678,1];
+
+let odd = function (){
+    let oddNumArray = [];
+    for(let i in numArray){
+    if(numArray[i]%2 != 0){
+     oddNumArray.push(numArray[i]);
+    }
+   }
+   console.log(oddNumArray);
+}
+odd();
+
+ //* Convert all the strings to title caps in a string array 
+ let capsArray = ["cat","dog","horse", "donkey"];
+ let caps = function(){
+      let tempArray = [];
+      for(let i in capsArray){
+          tempArray.push(capsArray[i][0].toUpperCase()+ capsArray[i].substring(1).toLowerCase() );
+      }
+      console.log(tempArray);
+
+ }
+ caps();
+
+// * Sum of all numbers in an array 
+let numVenamArray = [3,76,24,66,37,21,34,678,1];
+
+let sum = function (){
+    let temp =0;
+    for(let i in numVenamArray){
+     temp = (numVenamArray[i])+ temp;
+    
+   }
+   return temp;
+}
+console.log(sum());
+
+// * Return all the prime numbers in an array 
+let checkForPrime = function(inputNumber){
+let isItPrime = true;
+ 
+if(inputNumber <= 1) 
+{
+isItPrime = false;
+ 
+return isItPrime;
+}
+else
+{
+for (let i = 2; i<= inputNumber/2; i++) 
+{
+if ((inputNumber % i) == 0)
+{
+isItPrime = false;
+ 
+break;
+}
+}
+}
+return isItPrime;
+
+}
+
+ if(checkForPrime(11)){
+console.log("Prime number");
+}else{
+     console.log("Not a Prime number");
+} 
+
+// * Return all the palindromes in an array 
+
+// * Return median of two sorted arrays of same size 
+
+// * Remove duplicates from an array
+
+
+/*  Rotate an array by k times and return the rotated */
 // Use the  open weather api https://home.openweathermap.org to get the city weather information using latitude & longitude and city name 
 
 let request = new XMLHttpRequest ();
